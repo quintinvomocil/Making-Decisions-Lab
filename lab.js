55e2cfbe6cced03f99c6bab2fbd560a661482de5
Quintin Vomocil <quintinvomocil@gmail.com>
2022-06-15 15:41:34 -0600

    Solved through Q10

--- lab.js
+++ lab.js
@@ -158,10 +158,10 @@ let changeMyMind = true;
 
 if (changeMyMind = true) {
     changeMyMind = false}
-  console.log 
+  console.log(changeMyMind);
 
 ////////// PROBLEM 10 //////////
-// Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
+// Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9).After you change the value of changeMyMind, console.log it's new value (it should now read true).
 
 //CODE HERE
 
